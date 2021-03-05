8427bbaf736cde312653de1a3e740a8dff6ecb34
guptamohit0004 <mohit.gupta@accolitedigital.com>
2021-03-05 23:11:44 +0530

    Added add expense form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { AddExpenseComponent } from './add-expense/add-expense.component';
 import { LoginComponent } from './login/login.component';
 import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -16,16 +17,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeDashboardComponent,
     ManagerDashboardComponent,
+    AddExpenseComponent,
     NavBarComponent,
     LoginComponent,
     NotFoundComponent,
@@ -37,10 +42,13 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     MatInputModule,
     MatChipsModule,
+    FormsModule,
     MatCheckboxModule,
     MatSelectModule,
     MatIconModule,
+    MatDatepickerModule,
     ReactiveFormsModule,
+    MatNativeDateModule,
     AppRoutingModule,
   ],
   providers: [EmployeeService],
